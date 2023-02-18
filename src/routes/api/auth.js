72fd5ed4d9c3f0b5439f090f91f1f9af8bb7cfc2
tizenmile <tizen48@gmail.com>
2023-02-18 13:44:14 +0200

    auth problem

--- src/routes/api/auth.js
+++ src/routes/api/auth.js
@@ -10,14 +10,14 @@ const { auth } = require("../../middlewares/auth");
 
 const router = new express.Router();
 
-router.post(
-  "/register",
-  userValidation,
-  tryCatchWrapper(ctrlUser.registerUser)
-);
+// router.post(
+//   "/register",
+//   userValidation,
+//   tryCatchWrapper(ctrlUser.registerUser)
+// );
 
-router.post("/login", userValidation, tryCatchWrapper(ctrlUser.login));
+// router.post("/login", userValidation, tryCatchWrapper(ctrlUser.login));
 
-router.post("/logout", tryCatchWrapper(auth), tryCatchWrapper(ctrlUser.logout));
+// router.post("/logout", tryCatchWrapper(auth), tryCatchWrapper(ctrlUser.logout));
 
 module.exports = { authRouter: router };
--- src/routes/api/notices.js
+++ src/routes/api/notices.js
@@ -3,18 +3,18 @@ const router = express.Router();
 
 const { asyncWrapper } = require("../../helpers/apiHelpers");
 const  noticesControllers  = require("../../controllers");
-const { authMiddleware } = require("../../middlewares/authMiddleware");
+const {auth} = require("../../middlewares/auth");
 const noticeValidationMiddleware = require("../../middlewares/noticeValidationMiddleware");
 
 router.get("/:category",asyncWrapper(noticesControllers.getNoticesByCategoryController));
 router.get("/category/:category",asyncWrapper(noticesControllers.getNoticesByCategoryController));
-router.put("/add-to-fav/:id",authMiddleware,asyncWrapper(noticesControllers.addNoticeToFavoriteController));
-router.put("/del-from-fav/:id",authMiddleware,asyncWrapper(noticesControllers.delNoticeFromFavoriteController));
-router.get("/fav-notice/",authMiddleware,asyncWrapper(noticesControllers.getFavoriteNoticesController));
-router.get("/notice/:noticeId",authMiddleware,asyncWrapper(noticesControllers.getOneNoticeByIdController));
+router.put("/add-to-fav/:id",auth,asyncWrapper(noticesControllers.addNoticeToFavoriteController));
+router.put("/del-from-fav/:id",auth,asyncWrapper(noticesControllers.delNoticeFromFavoriteController));
+router.get("/fav-notice/",auth,asyncWrapper(noticesControllers.getFavoriteNoticesController));
+router.get("/notice/:noticeId",auth,asyncWrapper(noticesControllers.getOneNoticeByIdController));
 
 router.get("/owner/:own-notices",asyncWrapper(noticesControllers.getNoticesByOwnerController));
 router.delete("/:noticeId",asyncWrapper(noticesControllers.deleteNoticeByOwnerController));
-router.post("/notice",authMiddleware,noticeValidationMiddleware.postNoticeValidation,noticesControllers.addOneNoticeController);
+// router.post("/notice",auth,noticeValidationMiddleware.postNoticeValidation,noticesControllers.addOneNoticeController);
 
 module.exports = router;
