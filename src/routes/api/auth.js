821349d35e4c3e68d28eff0e6b17e31c829de5f8
tizenmile <tizen48@gmail.com>
2023-02-18 14:30:55 +0200

    auth back

--- src/routes/api/auth.js
+++ src/routes/api/auth.js
@@ -6,18 +6,16 @@ const ctrlUser = require("../../controllers/auth/");
 const { userValidation } = require("../../middlewares/validationMiddleware");
 const { auth } = require("../../middlewares/auth");
 
-// const { authMiddleware } = require("../../middlewares/validation/users/auth");
-
 const router = new express.Router();
 
-// router.post(
-//   "/register",
-//   userValidation,
-//   tryCatchWrapper(ctrlUser.registerUser)
-// );
+router.post(
+  "/register",
+  userValidation,
+  tryCatchWrapper(ctrlUser.registerUser)
+);
 
-// router.post("/login", userValidation, tryCatchWrapper(ctrlUser.login));
+router.post("/login", userValidation, tryCatchWrapper(ctrlUser.login));
 
-// router.post("/logout", tryCatchWrapper(auth), tryCatchWrapper(ctrlUser.logout));
+router.post("/logout", tryCatchWrapper(auth), tryCatchWrapper(ctrlUser.logout));
 
 module.exports = { authRouter: router };
