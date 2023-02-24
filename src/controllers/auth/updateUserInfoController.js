25ab6ea9b491b1d92c51ce967e02af0624f56c9a
Bohdan Pishchak <93521997+Stereoboi@users.noreply.github.com>
2023-02-24 15:35:27 +0200

    user_pers

--- src/controllers/auth/updateUserInfoController.js
+++ src/controllers/auth/updateUserInfoController.js
@@ -4,13 +4,9 @@ const updateUserInfoController = async (req, res) => {
   const { _id } = req.user;
   const result = await updateUserInfo(_id, req.body);
   const { avatarURL, name, email, mobile, city, birthday } = result;
+  console.log(result);
   res.status(200).json({
-    avatarURL: `${avatarURL}`,
-    name: `${name}`,
-    email: `${email}`,
-    mobile: `${mobile}`,
-    city: `${city}`,
-    birthday: `${birthday}`,
+    result,
   });
 };
 
