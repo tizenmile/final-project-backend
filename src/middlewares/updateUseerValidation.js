90fa45ac99a623e6c1939478dc11cd18d9922313
Bohdan Pishchak <93521997+Stereoboi@users.noreply.github.com>
2023-02-28 04:38:51 +0200

    user_pets

--- src/middlewares/updateUseerValidation.js
+++ src/middlewares/updateUseerValidation.js
@@ -4,22 +4,22 @@ const { HttpError } = require("../helpers/errors");
 const updateUserValidation = (req, res, next) => {
   const schema = Joi.object({
     email: Joi.string()
-      .pattern(new RegExp("^[a-zA-Z][0-9a-zA-Z_-]{1,}@(.+).(.+)$"))
-      .email({
-        minDomainSegments: 2,
-      })
-      .min(6)
-      .max(63),
-    name: Joi.string().min(2).max(15),
+      .email()
+      .min(10)
+      .max(63)
+      .pattern(
+        /^[a-zA-Z0-9._][a-zA-Z0-9._-]{0,}[a-zA-Z0-9._-]{0,}[a-zA-Z0-9._]@([a-zA-Z0-9.-]+\.)+[a-zA-Z]{2,}$/
+      ),
+    name: Joi.string().min(2).max(16),
     birthday: Joi.string().pattern(/^(\d{2})\.(\d{2})\.(\d{4})$/),
-    city: Joi.string().min(2).max(15),
+    city: Joi.string().min(2).max(60),
     mobile: Joi.string().pattern(new RegExp("^[+](380)[0-9]{9}$")),
   }).required();
 
   const { error } = schema.validate(req.body);
 
   if (error) {
-    return next(new HttpError(error.message));
+    return res.status(400).json(error.message);
   }
 
   next();
--- src/routes/api/notices.js
+++ src/routes/api/notices.js
@@ -7,7 +7,7 @@ const { auth } = require("../../middlewares/auth");
 const {
   postNoticeValidation,
 } = require("../../middlewares/noticeValidationMiddleware");
-const uploadCloud = require("../../middlewares/uploadMiddleware");
+const fileUploadMiddleware = require("../../middlewares/uploadMiddleware");
 
 router.get(
   "/category/:category",
@@ -46,7 +46,7 @@ router.delete(
 router.post(
   "/notice",
   auth,
-  uploadCloud.single("image"),
+  fileUploadMiddleware,
   postNoticeValidation,
   asyncWrapper(noticesControllers.addOneNoticeController)
 );
